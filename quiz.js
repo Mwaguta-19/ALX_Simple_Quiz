67ab9c0da6ace035648ca62572a85f861872f5d3
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-01-19 18:42:04 +0300

    files added

--- quiz.js
+++ quiz.js
@@ -3,21 +3,23 @@ function checkAnswer() {
     const correctAnswer = "4";
 
     // Get the selected answer from the radio buttons
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = document.querySelector('input[name="quiz"]:checked');
 
     // Get the feedback element
     const feedbackElement = document.getElementById('feedback');
 
     if (userAnswer) {
         // Compare the user's answer with the correct answer
-        if (userAnswer === correctAnswer) {
+        if (userAnswer.value === correctAnswer) {
             feedbackElement.textContent = "Correct! Well done.";
         } else {
             feedbackElement.textContent = "That's incorrect. Try again!";
         }
+        
     } else {
         feedbackElement.textContent = "Please select an answer.";
     }
+    
 }
 
 // Event listener to the submit button
