397ff81f3b256b5b39f09f93909173a264c823d1
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-01-19 23:51:35 +0300

    files rectified

--- quiz.js
+++ quiz.js
@@ -3,20 +3,20 @@ function checkAnswer (){
     const correctAnswer = "4";
 
     // Get the selected answer from the radio buttons
-    let userAnswer = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
 
     // Get the feedback element
     const feedbackElement = document.getElementById('feedback');
 
     // Check if an answer is selected
-    if (!userAnswer) {
+    /*if (!userAnswer) {
         // If no answer is selected, prompt the user to select one
         feedbackElement.textContent = "Please select an answer.";
         return; // Exit the function early if no answer is selected
-    }
+    }*/
 
     // Compare the user's answer with the correct answer
-    if (userAnswer.value === correctAnswer) {
+    if (userAnswer === correctAnswer) {
         feedbackElement.textContent = "Correct! Well done.";
     } else {
         feedbackElement.textContent = "That's incorrect. Try again!";
